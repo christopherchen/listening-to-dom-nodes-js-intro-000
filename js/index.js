81acf16accdd49edaecc75fbd1b6093027f5e25a
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-13 19:57:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,6 +8,13 @@ main.addEventListener('click', function(event) {
 
 const input = document.querySelector('input');
  
-input.addEventListener('keydown', function(e) {
-  console.log(e.key);
+input.addEventListener('keydown', 
+function(e) {
+  if (e.key === "g") {
+    return e.preventDefault();
+  } else {
+    console.log(e.key);
+  }
 });
+
+

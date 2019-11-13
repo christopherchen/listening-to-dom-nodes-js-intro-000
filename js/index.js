adc61db070d8925069831c57f71744c8f889ce27
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-13 20:07:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,4 +17,12 @@ function(e) {
   }
 });
 
-
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
